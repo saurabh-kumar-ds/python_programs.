81d1867e7a3f3a599d8f8ad2090d6c1b2164b03e
saurabh kumar <saurabh.comds@gmail.com>
2026-08-19 22:38:08 +0530

    add 3rd line for user awearness

--- mul.py
+++ mul.py
@@ -1,5 +1,6 @@
 ## Take two input from user to multiplecation.
 
+print("Enter two numbers for Multiplecation.")
 a = eval(input("Enter 1st number : "))
 b = eval(input("Enter 2nd number : "))
 print(f"Multiplecation of both numbers {a*b}")
