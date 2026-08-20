960fdba0a2ed1a5c7e52dcb88d5192d3d02d5ca1
saurabh kumar <saurabh.comds@gmail.com>
2026-08-20 08:16:07 +0530

    create a user define function add()

--- sum.py
+++ sum.py
@@ -1,5 +1,11 @@
-## take two numbers input from user and sum of those numbers.
+## take two numbers input from user and calculate sum of those numbert to use of function.
 
-a= int(input("enter 1st number : "))
-b= int(input("enter  2nd number : "))
-print(f"sum of both numbers = {a+b}")
+def add():
+    print("Enter two numbers for addition.")
+    a=eval(input("Enter 1st number : "))
+    b=eval(input("Enter 2nd number : "))
+    return a+b
+
+sum = add()
+print(f"Sum of both numbeers is : {sum}")
+
