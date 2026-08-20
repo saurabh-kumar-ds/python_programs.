83c96867d464a829a6d12f205fe7cd954c8a9be6
saurabh kumar <saurabh.comds@gmail.com>
2026-08-20 11:02:09 +0530

    add feature 'both are equal'

--- bigno.py
+++ bigno.py
@@ -1,10 +1,12 @@
 ## take two input from user to find biggest number.
 
-print("Enter two numbers to find biggest number.")
+print("Enter three numbers to find biggest number.")
 num1 = eval(input("Enter 1st number : "))
 num2 = eval(input("Enter 2nd number : "))
 
 if num1 > num2:
     print(f"biggest number is {num1}")
-else:
+elif (num1<num2):
     print(f"biggest number is {num2}")
+else: 
+    print("Both are equal")
