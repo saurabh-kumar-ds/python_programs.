d7d0cffb00b3b58d793a2ff946d0d9511e23eeee
saurabh kumar <saurabh.comds@gmail.com>
2026-08-21 06:54:58 +0530

    write a program to find max number

---
+++ max.py
@@ -0,0 +1,12 @@
+## enter two number to find maximum number.abs
+
+print("Enter two numbers to find maximum number.")
+a=eval(input("Enter 1st number : "))
+b=eval(input("Enter 2nd number : "))
+
+if b>a:
+    a=a+b
+    b=a-b
+    a=a-b
+
+print(f"Maximum number is {a}")
